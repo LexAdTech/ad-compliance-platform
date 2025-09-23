5b8177f465b1e60545544a2dbe9fac96845376ab
Vadim Manakov <manakov_vadim2004@mail.ru>
2025-09-23 15:43:20 +0300

    created spring boot app

---
+++ advertisement-analyzer/src/main/java/com/konsultantplus/project/adanalyzer/advertisementanalyzer/AdvertisementAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.konsultantplus.project.adanalyzer.advertisementanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertisementAnalyzerApplication.class, args);
+    }
+
+}
