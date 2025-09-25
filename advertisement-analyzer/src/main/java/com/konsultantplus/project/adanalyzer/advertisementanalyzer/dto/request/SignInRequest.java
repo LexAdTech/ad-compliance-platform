863096a5d41da41366e5ebe47e0dad5a8465fad3
Vadim Manakov <manakov_vadim2004@mail.ru>
2025-09-25 16:31:17 +0300

    документация

--- advertisement-analyzer/src/main/java/com/konsultantplus/project/adanalyzer/advertisementanalyzer/dto/request/SignInRequest.java
+++ advertisement-analyzer/src/main/java/com/konsultantplus/project/adanalyzer/advertisementanalyzer/dto/request/SignInRequest.java
@@ -1,9 +1,14 @@
 package com.konsultantplus.project.adanalyzer.advertisementanalyzer.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Запрос на авторизацию пользователя")
 public class SignInRequest {
+    @Schema(description = "Имя пользователя", example = "testuser", required = true)
     private String username;
+
+    @Schema(description = "Пароль", example = "mySecurePassword123", required = true)
     private String password;
 }
