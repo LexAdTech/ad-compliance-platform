5f8d1b2bbeb1c9dbc016291e14518e05f11851b1
Vadim Manakov <manakov_vadim2004@mail.ru>
2025-10-14 18:32:03 +0300

    fix front

--- advertisement-analyzer/src/main/java/com/konsultantplus/project/adanalyzer/advertisementanalyzer/AdvertisementAnalyzerApplication.java
+++ advertisement-analyzer/src/main/java/com/konsultantplus/project/adanalyzer/advertisementanalyzer/AdvertisementAnalyzerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdvertisementAnalyzerApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(AdvertisementAnalyzerApplication.class, args);
     }
